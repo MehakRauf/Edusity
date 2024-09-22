f4d958cb1e00a061a0b6c665e6ca8cc34067e8ed
Mehak Fatima <mehak.fatima2386@gmail.com>
2024-09-22 17:58:28 +0500

    Done with the program section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Title subTitle='OUR PROGRAM' title='What We Offer'/>
       <Programs />
     </div>
   )
