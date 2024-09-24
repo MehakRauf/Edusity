85367d57d5022cd72f34e064026f77ff1a0602f1
Mehak Fatima <mehak.fatima2386@gmail.com>
2024-09-24 11:16:58 +0500

    done with campus section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/campus/Campus'
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
       <Programs />
       <About />
       <Title subTitle='Gallery' title='Campus Photos' />
+      <Campus/>
+      <Title subTitle='TESTIMONIALS' title='What Student Says' />
     </div>
   )
 }
