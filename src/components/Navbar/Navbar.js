7ab580dbf38b96b7b6af1f2d64241c683ba3feba
Mehak Fatima <mehak.fatima2386@gmail.com>
2024-09-27 12:17:38 +0500

    made the navbar section responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
+import menu_icon from '../../assets/menu-icon.png'
 import logo from '../../assets/logo.png';
 
 const Navbar = () => {
+  const [showMenu, SetShowMenu] = useState(false);
+  const toggleMenu = () => {
+    showMenu ? SetShowMenu(false) : SetShowMenu(true);
+  }
   const [sticky, setSticky] = useState(false);
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -13,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ""}`}>
       <img className='logo' src={logo} alt="" />
-      <ul>
+      <ul className={showMenu? '' : 'hide-mobile-menu'}>
         <li><Link to="hero" smooth={true} offset={0} duration={500} >Home</Link></li>
         <li><Link to="programs" smooth={true} offset={-260} duration={500} >Programs</Link></li>
         <li><Link to="about" smooth={true} offset={-150} duration={500} >About us</Link></li>
@@ -21,6 +26,7 @@ const Navbar = () => {
         <li><Link to="testimonials" smooth={true} offset={-260} duration={500} >Testimonials</Link></li>
         <li><Link to="contact" smooth={true} offset={-240} duration={500} className='btn' >Contact us</Link></li>
       </ul>
+      <img src={menu_icon} className='menu-icon' alt="" onClick={toggleMenu} />
     </nav>
   )
 }
